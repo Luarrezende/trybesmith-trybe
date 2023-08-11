b8542bfac9279d33f3cd61b922cd669960b7c5dc
Luar <luarrezende@gmail.com>
2023-08-11 17:37:59 -0300

    resolve uma parte de teste da products service

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,26 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import productsService from '../../../src/service/productsService';
 
 describe('ProductsService', function () {
+
   beforeEach(function () { sinon.restore(); });
 
+  it('should handle product creation error', async function () {
+    const createStub = sinon.stub(ProductModel, 'create');
+    createStub.rejects(new Error('Database error'));
+
+    const result = await productsService.createProduct({
+      id: 1,
+      name: 'Mesa',
+      price: '55.99',
+      orderId: 1,
+    });
+
+    expect(result).to.deep.equal({
+      status: 'INVALID_DATA',
+      data: { message: 'Could not create product' },
+    });
+  });
 });
