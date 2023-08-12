d06d6c0edeff6042a3f7e42398f8459f43cd45e9
Luar <luarrezende@gmail.com>
2023-08-12 18:03:18 -0300

    teste de integracao create

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
-import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import app from '../../../src/app'
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('teste camada Post', async function() {
 
+    const mockProduct = { name: 'Mesa', price: '90.65', orderId: 8 }
+    const productBuild = ProductModel.build(mockProduct)
+    
+    sinon.stub(ProductModel, 'create').resolves(productBuild)
+    const response = await chai.request(app).post('/products').send(mockProduct)
+
+    expect(response.body).to.be.an('object')
+    expect(response.status).to.be.equal(201)
+  })
 });
